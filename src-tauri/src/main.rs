c1d67bb93f9eb5f04b73038cff1dcb9807067fd1
zuiyu1998 <1542844298@qq.com>
2024-06-26 11:59:39 +0800

    feat: 更新AppState的初始化

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -4,12 +4,21 @@
 mod error;
 mod project;
 
+use api::abi::config::Config;
+use api::abi::tokio;
 use api::State as AppState;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let config = Config::default();
+
+    let state = AppState::from_config(&config)
+        .await
+        .expect("state init fail.");
+
     tauri::Builder::default()
         .plugin(tauri_plugin_shell::init())
-        .manage(AppState::new())
+        .manage(state)
         .invoke_handler(tauri::generate_handler![
             project::get_project_list,
             project::excute_project
