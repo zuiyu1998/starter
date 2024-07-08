581b17fda3e0c5e28e5e428e948287d6414fa6e0
zuiyu1998 <1542844298@qq.com>
2024-07-08 11:04:25 +0800

    feat: 添加更多的图标

--- src/components/Icon/icons.ts
+++ src/components/Icon/icons.ts
@@ -9,6 +9,12 @@ function lazy(importFn: Function) {
 
 export const icons = {
   godot: lazy(async () => import('../../assets/icons/godot.svg?react')),
+  react: lazy(async () => import('../../assets/icons/react.svg?react')),
+  react_native: lazy(
+    async () => import('../../assets/icons/react_native.svg?react')
+  ),
+  vue: lazy(async () => import('../../assets/icons/vue.svg?react')),
+  rust: lazy(async () => import('../../assets/icons/rust.svg?react')),
 };
 
 export const iconData = Object.keys(icons);
