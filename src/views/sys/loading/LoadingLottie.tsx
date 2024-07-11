7628856d8a9c1314c2c0469acef336e6fbe1cb93
zuiyu1998 <1542844298@qq.com>
2024-07-11 10:43:31 +0800

    fix: git 区分大小写

---
+++ src/views/sys/loading/LoadingLottie.tsx
@@ -0,0 +1,32 @@
+import { useRef, useImperativeHandle, forwardRef } from 'react';
+import animationData from '/@/assets/lotties/loading.json';
+import { Player } from '@lottiefiles/react-lottie-player';
+import './index.less';
+
+export interface LoadingLottieRef {
+  setSeeker(seek: number): void;
+}
+
+export const LoadingLottie = forwardRef<LoadingLottieRef, unknown>(
+  function LoadingLottie(_props, ref) {
+    const playerRef = useRef<Player>(null);
+
+    function setSeeker(seek: number) {
+      playerRef.current?.setSeeker(seek);
+    }
+
+    useImperativeHandle(
+      ref,
+      () => {
+        return {
+          setSeeker,
+        };
+      },
+      []
+    );
+
+    return (
+      <Player ref={playerRef} src={animationData} className='h-full w-full' />
+    );
+  }
+);
